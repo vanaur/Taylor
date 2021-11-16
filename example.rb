68fc2eb5d0bdc4048f168d9a8a6854ccb2e8025f
vanaur <28791317+vanaur@users.noreply.github.com>
2021-11-16 23:17:21 +0100

    Create example.rb

---
+++ example.rb
@@ -0,0 +1,13 @@
+# Un exemple d'utilisation, ce qui suit un symbole `#` est un commentaire
+
+Function: F = m * (v / t) # La fonction théorique (variante de la 2' loi de Newton)
+
+Constant: m = 10 # Masse constante (g)
+
+Error: m = 0.12 # On utilise un logiciel, c'est son erreur
+Error: v = 0.12 # Idem
+Error: t = 0.2  # On utilise un chronomètre
+
+# On a fait 10 mesures
+Measures: v = 1.3, 1.8, 2.4, 2.8, 3.2, 3.9, 4.6, 5.0, 5.6, 6.3 # (m/s)
+Measures: t = 1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  , 10  # (s)
